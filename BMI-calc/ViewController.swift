f422f510fd82754e2b45660de8a52f76713ea8c8
FischerAlx <fisheralx@icloud.com>
2022-12-18 23:38:40 +0100

    Initial Commit

---
+++ BMI-calc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-calc
+//
+//  Created by Александр Фишер on 18.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
