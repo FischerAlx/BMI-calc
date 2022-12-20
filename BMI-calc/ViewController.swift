beec56b93a4ec32c49c452bf5f09df23e236c05b
FischerAlx <fisheralx@icloud.com>
2022-12-20 20:36:44 +0100

    connect via IBOutlet and IBAction

--- BMI-calc/ViewController.swift
+++ BMI-calc/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var currentHeightLabel: UILabel!
+    @IBOutlet weak var currentWeightLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func pressedCalcButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    @IBAction func changedHeightSlider(_ sender: UISlider) {
+        
+        
+        
+    }
+    
+    @IBAction func changedWeightSlider(_ sender: UISlider) {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
